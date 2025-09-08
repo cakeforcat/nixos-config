71c49741cc186bde948f3af8dd61ced4e7eadbdc
cakeforcat <cakeforcat@gmail.com>
2025-09-09 00:40:41 +0100

    better versioning

--- holy-mother-of-scripts.fish
+++ holy-mother-of-scripts.fish
@@ -166,7 +166,7 @@ set -l curr_json (nixos-rebuild list-generations --json | jq -r '.[] | select (.
 set -l curr_generation (echo $curr_json | jq -r '"\(.generation)"')
 # set -l curr_date (echo $curr_json | jq -r '"\(.date)"')
 set -l curr_nixos (echo $curr_json | jq -r '"\(.nixosVersion)"')
-set -l curr_nixos_major (echo $curr_nixos | string split -f "1" -m 2 -r .)
+set -l curr_nixos_major (echo $curr_nixos | string split -f "1,2" . | string join .)
 set -l curr_kernel (echo $curr_json | jq -r '"\(.kernelVersion)"')
 git add npins/sources.json
 git add *.nix
