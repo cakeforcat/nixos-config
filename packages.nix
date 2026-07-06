0773f5d16ca11eb8f45985a616a5a6809a93f1c9
cakeforcat <julia@cakeforcat.dev>
2026-07-06 12:29:07 +0100

    Gen: 291 NixOS: 26.11pre1027867 Kernel: 6.18.38

--- packages.nix
+++ packages.nix
@@ -169,21 +169,4 @@
     totem
     showtime
   ];
-
-  nixpkgs.overlays = [
-
-    (final: prev: {
-      pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-        (python-final: python-prev: {
-          mpv = python-prev.mpv.overridePythonAttrs (oldAttrs: {
-            disabledTests = [ "RegressionTests::test_wait_for_property_concurrency" ];
-          });
-        })
-      ];
-    })
-
-    (final: _prev: {
-      pnpm_10_29_2 = final.pnpm_10;
-    })
-  ];
 }
